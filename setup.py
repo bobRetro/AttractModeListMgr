755a635e6ffeae95776546d49d46a1b87f228958
bobRetro <76525464+bobRetro@users.noreply.github.com>
2021-02-04 10:58:32 -0600

    Renamed project to AttractModeListMgr

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 import py2exe
 
-setup(windows=[{"script":"AttractModeMameListMgr.py"}],
+setup(windows=[{"script":"AttractModeListMgr.py"}],
     options={"py2exe":{"includes":["PyQt5.sip", "PyQt5", "PyQt5.QtCore", "PyQt5.QtGui"]}},
     data_files = [("platforms", ["C:\\Users\\Video\\AppData\\Local\\Programs\\Python\\Python39\\Lib\\site-packages\\PyQt5\\Qt\\plugins\\platforms\\qwindows.dll"]),
                   ("imageformats",["C:\\Users\\Video\\AppData\\Local\\Programs\\Python\\Python39\\Lib\\site-packages\\PyQt5\\Qt\\plugins\\imageformats\\qicns.dll",
